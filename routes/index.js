82b359bc13087febfcc66b222fcd5c7ea05c7fb6
Ruchi <r144@my.centennialcollege.ca>
2019-03-23 01:55:11 -0400

    added ExpressJS support

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+/* GET About page. */
+router.get("/about", function(req, res, next) {
+  res.render("index", { title: "About" });
+});
+
+/* GET products page. */
+router.get("/products", function(req, res, next) {
+  res.render("index", { title: "Services" });
+});
+
+/* GET services page. */
+router.get("/services", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/* GET contact page. */
+router.get("/contact", function(req, res, next) {
+  res.render("index", { title: "Contact" });
+});
+
 module.exports = router;
